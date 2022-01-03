de400e0e934953e9694f6605fd3c8921a210733d
NABI <nabiabib31@gmail.com>
2022-01-03 11:02:32 +0100

    aouter le dossier mes Documents qui contien les cas d'utilisations

---
+++ Documents/Mon projet NABI/3_Travail_de_Groupe/MoazRudyFabrice/tp_PhiloLoisirsmaison/tp1exo4.groovy
@@ -0,0 +1,62 @@
+Exo 4 : 
+gestion d’un Club de Loisirs
+Le Club de Loisirs Municipal  souhaite informatiser la gestion de son activité,
+et vous demande de concevoir une application Web assurant les fonctionnalités suivantes :
+
+Côté back office :
+Le futur logiciel, baptisé PhiloLoisirs®  doit permettre
+à la secrétaire du Club de créer et gérer le catalogue des activités
+(solfège, chant, arts graphiques, yoga, poterie …)  proposées sur le site
+internet du club.
+Il doit également permettre au comptable de gérer les règlements
+des abonnements des adhérents aux activités (remises de chèques, échéanciers, impayés…)
+Côté front office :
+PhiloLoisirs® doit permettre aux internautes de consulter
+la liste des activités proposées et de choisir un ou plusieurs
+abonnements et remplir un panier. Mais pour valider leur commande ils doivent
+avoir un compte et s’y connecter
+PhiloLoisirs® doit permettre aux adhérents de s’abonner aux activités proposées : 
+Seuls les adhérents à PhiloLoisirs® peuvent valider un panier
+contenant un (ou plusieurs) abonnement(s)t à une ou plusieurs activités 
+et le régler en ligne.
+Pour être adhérent il faut avoir créé son compte sur le site via l’application PhiloLoisirs®.
+L’adhérent doit s’être connecté avec son login pour pouvoir
+valider son panier, payer en ligne, gérer son compte, visualiser l’historique le concernant,
+bref pour toutes les actions qui lui sont propres.
+Chaque abonnement est annuel et a un tarif spécifique
+suivant l’activité choisie.
+Le paiement en ligne doit être proposé, mais Il reste possible
+de payer le montant de l’abonnement par chèque(s) remis au comptable
+du Club PhiloLoisirs® (par courrier ou directement à la permanence administrative).
+Il est possible de régler le montant d’un abonnement en plusieurs fois,
+avec plusieurs chèques, en indiquant la date de dépôt souhaitée pour chaque chèque.
+Travail demandé :
+Diagramme fonctionnel (statique)
+Elaborez le diagramme des cas d’utilisation de la future application PhiloLoisirs®
+Diagramme structurel (statique)
+Elaborez le diagramme de classes de la future application PhiloLoisirs®
+
+
+Acteur 
+    Secrétaire
+    Client
+    Visiteur
+    Comptable
+
+
+Verbe d'action
+    Consulter (pour tous le monde)
+    Créer et gérer les activités
+    Gérer les règlements
+    Choisir
+    Remplir le panier (client, visiteur)
+    Authentififier
+    Valider (client)
+    Payer
+    S'inscrire
+    Se connecter
+
+
+
+
+
